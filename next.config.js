2cd358a7852cd123718d6e08e20b30fe8de30b00
Alex Jones <alex@jellybaby.com>
2024-06-23 19:02:58 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // next.config.js
 module.exports = {
   reactStrictMode: true,
@@ -5,3 +6,28 @@ module.exports = {
     domains: ['minotar.net'], // Add your specific hostname here
   },
 };
+=======
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  transpilePackages: ["geist"],
+};
+
+export default config;
+>>>>>>> f8e4276 (initial)
